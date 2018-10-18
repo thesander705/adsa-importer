3a886f13adb700589a915d9fda6f62db9a8472bb
Unknown <sander.de.boer4798@hotmail.com>
2018-10-18 12:35:39 +0200

    Customers and products are running in multi thread.

--- AdsaImporter/AdsaImporter/Program.cs
+++ AdsaImporter/AdsaImporter/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using AdsaImporter.Models;
 
@@ -11,18 +12,19 @@ namespace AdsaImporter
     {
         static void Main(string[] args)
         {
+            WaitHandle[] waitHandles = {
+                new AutoResetEvent(false),
+                new AutoResetEvent(false)
+            };
+
+            ThreadPool.QueueUserWorkItem(DoCustomers, waitHandles[0]);
+            ThreadPool.QueueUserWorkItem(DoProducts, waitHandles[1]);
+
+
+            WaitHandle.WaitAll(waitHandles);
+
             Importer importer = new Importer();
             Exporter exporter = new Exporter();
-
-            Console.WriteLine("Start importing customer");
-            List<Customer> customers = importer.ImportCustomers();
-            Console.WriteLine("Start exporting customer");
-            exporter.ExportCustomers(customers);
-
-            Console.WriteLine("Start importing products");
-            List<Product> products = importer.ImportProducts();
-            Console.WriteLine("Start exporing products");
-            exporter.ExportProducts(products);
 
             Console.WriteLine("Start importing orders");
             List<Order> orders = importer.ImportOrders();
@@ -30,6 +32,39 @@ namespace AdsaImporter
             Console.WriteLine("Start exporting orders");
             exporter.ExportOrders(orders);
 
+            Console.WriteLine("Finished");
+            Console.ReadLine();
+
+        }
+
+        private static void DoCustomers(object state)
+        {
+            Importer importer = new Importer();
+            Exporter exporter = new Exporter();
+
+            Console.WriteLine("Start importing customers");
+            List<Customer> customers = importer.ImportCustomers();
+            Console.WriteLine("Start exporting customers");
+            exporter.ExportCustomers(customers);
+            Console.WriteLine("Exporting customers finished");
+
+            AutoResetEvent waitHandle = (AutoResetEvent) state;
+            waitHandle.Set();
+        }
+
+        private static void DoProducts(object state)
+        {
+            Importer importer = new Importer();
+            Exporter exporter = new Exporter();
+
+            Console.WriteLine("Start importing products");
+            List<Product> products = importer.ImportProducts();
+            Console.WriteLine("Start exporing products");
+            exporter.ExportProducts(products);
+            Console.WriteLine("finished exporing products");
+
+            AutoResetEvent waitHandle = (AutoResetEvent)state;
+            waitHandle.Set();
         }
     }
 }
